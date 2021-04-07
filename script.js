bba24b979df169f141ca05c85b16b61c7c35e093
Alberto Gambino <gambafb@libero.it>
2021-04-07 17:10:42 +0100

    Slider Vue + bonus

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function sliderVue (){
         el: '#app',
         data:{
           images:[
-          "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
-          "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
-          "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
-          "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
-        ],
-        timer: null,
-        currentIndex: 0
+              "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+              "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+              "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+              "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+          ],
+          timer: null,
+          currentIndex: 0
         },
         mounted: function(){
           this.startSlide();
@@ -32,13 +32,15 @@ function sliderVue (){
           },
 
 
-          }
-          computed: {
+        },
+        computed: {
           currentImg: function() {
           return this.images[Math.abs(this.currentIndex) % this.images.length];
         }
-  })
+  }
+})
 }
+
 
 
 
